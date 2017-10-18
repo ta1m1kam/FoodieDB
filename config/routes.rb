a58769e3f538a64c8004447d435a42c88e49e065
TaigaMikami <mikamimikami60@gmail.com>
2017-10-19 01:16:51 +0900

    画像の受け釣るためのinputタグを挿入せねば

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+  post 'users/create' => 'users#create'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
+
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017172810) do
+ActiveRecord::Schema.define(version: 20171018101008) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image_name"
   end
 
 end
