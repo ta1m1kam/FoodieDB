bb420da9f1b1b98d02bbedb2e1f95fc29aa18808
TaigaMikami <mikamimikami60@gmail.com>
2017-10-22 15:31:16 +0900

    いいね

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
     @user = User.find_by(id: @post.user_id)
+    @likes_count = Like.where(post_id: @post.id).count
   end
 
   def new
