0343b10ff606e4a7d878a6a4153e6110c16f94d3
TaigaMikami <mikamimikami60@gmail.com>
2017-10-22 22:39:19 +0900

    パスワードの暗号化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022052107) do
+ActiveRecord::Schema.define(version: 20171022133045) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20171022052107) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "image_name"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
 end
